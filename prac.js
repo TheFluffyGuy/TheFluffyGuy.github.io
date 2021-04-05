0b4e1a4bdefa85c2d21b82195e2279cc3001a740
TheFluffyGuy <77924733+TheFluffyGuy@users.noreply.github.com>
2021-04-05 17:07:32 +0800

    initial commit

---
+++ prac.js
@@ -0,0 +1,15 @@
+function myFunctios() {
+    var img = document.getElementsByClassName("imagelol");
+    var x = document.getElementById("ham");
+
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        img.style.display = "none";
+    } else {
+        x.style.display = "block";
+        x.style.display = "block";
+    }
+
+
+}
